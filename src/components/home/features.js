82f76e22f2c4e0e2a4191074bfc093b5f869fda9
Alejandro Vivas <alejandrojv17@gmail.com>
2020-03-31 09:58:26 -0300

    Home: Features

--- src/components/home/features.js
+++ src/components/home/features.js
@@ -7,41 +7,52 @@ const Features = () => {
         <section className="ui__section">
             <div className="container">
                 <div className="row justify-content-center">
-                    <div className="col-lg-10 col-sm-12">
+                    <div className="col-sm-12">
                         <div className="ui__section-content">
-                                <h2 className="ui__section-title text-center mywork mb-5">¿Cómo es mi trabajo?</h2>
+                                <h2 className="ui__section-title text-center mywork mb-5">¿Cómo trabajo?</h2>
                                 <div className={styles.f__box}>
 
                                     <div>
                                         <div className={styles.f__boxItem}>
+                                            <span className={styles.icon + ' ' + styles.icon__responsive}></span>
                                             <h4>DISEÑO RESPONSIVE</h4>
-                                            <p>Sitio Web 100% adaptativo. Tu sitio se verá bien tanto en PC como en dispositivos móviles.</p>
+                                            <p>Realizo diseños 100% adaptativos. Que se vean bien tanto en PC como en dispositivos móviles.</p>
                                         </div>
 
-                                        <div className={styles.f__boxItem}>
+                                        <div className={styles.f__boxItem + ' ' + styles.isDark}>
+                                        <span className={styles.icon + ' ' + styles.icon__modern}></span>
                                             <h4>DISEÑO MODERNO</h4>
-                                            <p>Obtendrás un trabajo de altura, calidad y además con las últimas tecnologías de desarrollo web.</p>
+                                            <p>Mantengo trabajo de altura, calidad y además con las últimas tecnologías de desarrollo web.</p>
                                         </div>
 
                                         <div className={styles.f__boxItem}>
+                                        <span className={styles.icon + ' ' + styles.icon__fast}></span>
                                             <h4>RÁPIDO Y ÓPTIMO</h4>
-                                            <p>La mejor experiencia de usuario está garantizada, tus clientes navegarán con la mejor rapidéz.</p>
+                                            <p>Garantizo la mejor experiencia de usuario, los usuarios navegarán con la mejor rapidéz.</p>
                                         </div>
                                     </div>
 
-                                    <div>
-                                        <img src="http://placehold.it/800x1200" className="img-fluid" alt="Site Mockup"/>
+                                    <div className={styles.media}>
+                                        <img src="https://cdn.dribbble.com/users/365424/screenshots/10827250/media/f69daf77f01a3c16018c66f5c8c55c19.png" className="img-fluid" alt="Site Mockup"/>
                                     </div>
 
                                     <div>
-                                        <div className={styles.f__boxItem + ' ' + styles.isDark}>
+                                    <div className={styles.f__boxItem}>
+                                            <span className={styles.icon + ' ' + styles.icon__search}></span>
                                             <h4>PARA BUSCADORES</h4>
-                                            <p>Listo para ser encontrado a través de los buscadores más populares y obtener presencia en Internet.</p>
+                                            <p>Sitios listos para ser encontrados a través de los buscadores más populares y obtener presencia en Internet.</p>
                                         </div>
 
                                         <div className={styles.f__boxItem + ' ' + styles.isDark}>
-                                            <h4>RÁPIDO Y ÓPTIMO</h4>
-                                            <p>La mejor experiencia de usuario está garantizada, tus clientes navegarán con la mejor rapidéz.</p>
+                                        <span className={styles.icon + ' ' + styles.icon__professional}></span>
+                                            <h4>DISEÑO PROFESIONAL</h4>
+                                            <p>Diseños y desarrollos por un profesional con mas de 6 años de experiencia en grandes proyectos.</p>
+                                        </div>
+
+                                        <div className={styles.f__boxItem}>
+                                        <span className={styles.icon + ' ' + styles.icon__professional}></span>
+                                            <h4>DISEÑO PROFESIONAL</h4>
+                                            <p>Diseños y desarrollos por un profesional con mas de 6 años de experiencia en grandes proyectos.</p>
                                         </div>
                                     </div>
 
