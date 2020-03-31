3d52886e1bcee5f5a0a1038ea2488de9cb6371e1
Alejandro Vivas <alejandrojv17@gmail.com>
2020-03-30 23:38:38 -0300

    Home: About section

--- src/components/home/about.js
+++ src/components/home/about.js
@@ -5,22 +5,21 @@ const About =  () => {
         <section className="ui__section isDark">
             <div className="container">
                 <div className="row">
-                    <div className="col-sm-6">
+                    <div className="col-sm-5">
                         <div className="ui__section-content">
                             <h2>Acerca de mí</h2>
 
-                            <p>Me llamo Alejandro Vivas, y he trabajado por mas de 6 años con Desarrollo Frontend y Diseño Web.</p>
-                            <p>Tengo experiencia trabajando con diferentes tecnologías para Diseño Web tales como, HTML5, CSS3/SASS, Javascript</p>
-
+                            <p>Me llamo Alejandro Vivas, y he trabajado por mas de 6 años con Desarrollo Frontend y Diseño Web. Tengo experiencia trabajando con diferentes tecnologías para Diseño Web tales como, <code>HTML5, CSS3/SASS, Javascript</code>.</p>
                             
-                            <img src="/img/tech-logos.png" className="img-fluid" alt="I love JavaScript"/>
-                            <h5 className="font-weight-light my-3 text-info">Demás está decir que mi lenguaje favorito es Javascript ❤</h5>
+                            <img src="/img/tech-logos.png" className="img-fluid d-block mt-4" alt="I love JavaScript"/>
+                            <small className="font-weight-light my-3 d-block text-center">Demás está decir que mi lenguaje favorito es Javascript ❤</small>
 
-                            <p className="mt-5"><strong>¿Herramientas de diseño?</strong> Manejo muy bien Adobe Photoshop, Illustrator, Experience Design, Balsamiq, entre otros.</p>
+                            <h5 className="mt-4 text-info">¿Herramientas de diseño?</h5>
+                            <p>Manejo muy bien Adobe Photoshop, Illustrator, Xperience Design, Balsamiq, entre otros. Me gusta mucho preocuparme por el Look & Feel de un producto, así como también mantener principios básicos de UI / UX.</p>
 
                         </div>
                     </div>
-                    <div className="col-sm-6">
+                    <div className="col-sm-7">
                         <div className="ui__section-content">
                             <img src="/img/carbon-react.png" className="img-fluid" alt="I love React"/>
                         </div>
