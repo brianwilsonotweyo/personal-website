53a023bcc8c0434de0577644a366b6e345da7067
Alejandro Vivas <alejandrojv17@gmail.com>
2020-04-02 09:19:58 -0300

    Remove Api Call from Projects Page

--- src/components/portfolio/PortfolioContainer.js
+++ src/components/portfolio/PortfolioContainer.js
@@ -1,47 +1,20 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import PortfolioItem from './portfolio-item';
-import { getProjects } from '../../services/api';
+
+import data from './data.json';
 
 import styles from './portfolio.module.scss';
 
-const Spinner = () => {
-    return  (
-        <div className="d-flex justify-content-center">
-            <div className="spinner-border" role="status">
-                <span className="sr-only">Loading...</span>
-            </div>
-        </div>
-    )
-}
-
 const PortfolioContainer = () => {
-    const [loading, setLoading] = useState(false);
-    const [items, setItems] = useState([]);
 
-    
-    const getPorfolio = () => {
-        setLoading(true)
-    
-        getProjects().then(res => {
-            const array = res.projects.reverse();
-            setItems(array)
-            setLoading(false)
-        }).catch(err => {
-            setLoading(false);
-            console.log('Error', err);
-        });
-    }
+    const items = data.projects.reverse();
 
-    useEffect(() => {
-        getPorfolio();
-    }, []);
+    const filteredItems = items.filter(i => i.published);
     
-
-    return loading ? <Spinner/> : 
+    return (
     <div className={styles.p__grid}>
         {
-            loading ? <Spinner/> : 
-            items.map(i => <PortfolioItem
+            filteredItems.map(i => <PortfolioItem
                     photo={i.photo}
                     thumb={i.thumb}
                     name={i.name}
@@ -51,7 +24,7 @@ const PortfolioContainer = () => {
                     published={i.published}
                     key={i.id} />)
         }
-    </div>
+    </div>)
 }
 
 export default PortfolioContainer;
