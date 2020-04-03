9f4b7586a44fca013db7d82c9d96419f2f7f394a
Alejandro Vivas <alejandrojv17@gmail.com>
2020-04-03 17:01:52 -0300

    Add blog cover

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Link, graphql, useStaticQuery } from 'gatsby';
+import Img from "gatsby-image"
 
 import Layout from '../components/shared/layout';
 import SEO from '../components/seo';
 import Sidebar from '../components/shared/sidebar';
+
 
 import styles from './blog.module.scss';
 
@@ -21,7 +23,20 @@ const Blog =() => {
           node {
             title
             slug
+            excerpt
+            cover {
+
+              fluid(maxWidth: 350, maxHeight: 196, resizingBehavior: SCALE) {
+              ...GatsbyContentfulFluid_tracedSVG
+            }
+              file {
+                url
+              }
+            }
             publishedDate(formatString: "MMMM Do, YYYY")
+            body {
+              json
+            }
           }
         }
       }
@@ -50,13 +65,25 @@ const Blog =() => {
 
                     {
                       posts.map((item, index) => (
-                        <Link  to={`/blog/${item.node.slug}`} className={styles.blog__post}>
-                          <div className={styles.blog__postCover}>
-                              <img src="http://placehold.it/400x180" alt={styles.blog__postTitle}/>
-                          </div>
+                        <Link key={index}  to={`/blog/${item.node.slug}`} className={styles.blog__post}>
+
+                          {item.node.cover.fluid.src && (
+                            <div className={styles.blog__postCover}>
+                              <Img
+                                fluid={item.node.cover.fluid}
+                                alt={styles.blog__postTitle}
+                              />
+                            </div>
+                          )}
+                          
                           <h2 className={styles.blog__postTitle}>
                                 {item.node.title}
                           </h2>
+                          
+                          {
+                            item.node.excerpt && <p className={styles.blog__postExcerpt}>{item.node.excerpt}</p>
+                          }
+                          
                           <div className={styles.blog__postMetas}>
                             {item.node.publishedDate}
                           </div>
