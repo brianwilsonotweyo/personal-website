73046049b5c36031f40e498c645a1468e9a6ad9f
Alejandro Vivas <ajvivas86@gmail.com>
2020-04-05 16:51:18 -0400

    Create and apply useContext in some areas

--- src/components/shared/layout.js
+++ src/components/shared/layout.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { useStaticQuery, graphql } from "gatsby"
+
 import Header from './header';
 import Footer from './footer';
 
@@ -9,11 +11,38 @@ import '../../assets/scss/main.scss';
 
 import styles from './layout.module.scss';
 
+export const ConfigContext = React.createContext();
+
 const Layout = ({location, title, children, wided}) => {
+
+    const {site} = useStaticQuery(graphql`
+    query {
+        site {
+            siteMetadata {
+                title
+                description
+                author
+                twitter
+                github
+                linkedin
+            }
+        }
+    }
+    `)
+
+    const siteContext = {
+        title: site.siteMetadata.title,
+        description: site.siteMetadata.description,
+        author: site.siteMetadata.author,
+        twitter: site.siteMetadata.twitter,
+        linkedin: site.siteMetadata.linkedin,
+        github : site.siteMetadata.github
+    }
 
     const container = wided ? styles.containerLayout : 'container ' + styles.containerLayout;
     return ( 
         <>
+        <ConfigContext.Provider value={siteContext}>
             <Header/>
             <div className={container}>
                 <div className={styles.content}>
@@ -21,6 +50,7 @@ const Layout = ({location, title, children, wided}) => {
                 </div>
                 <Footer/>
             </div>
+        </ConfigContext.Provider>
         </>
      );
 }
--- src/components/shared/social-list.js
+++ src/components/shared/social-list.js
@@ -1,36 +1,25 @@
-import React from 'react';
-import { graphql, useStaticQuery } from 'gatsby';
+import React, { useContext } from 'react';
+import { ConfigContext } from './layout';
 import styles from './social-list.module.scss';
 
-
 const SocialList = () => {
-    const data = useStaticQuery(graphql`
-    query {
-        site {
-            siteMetadata {
-                author
-                twitter
-                github
-                linkedin
-            }
-        }
-    }
-    `)
+
+    const context = useContext(ConfigContext);
 
     return (
         <ul className={styles.socialList}>
             <li>
-                <a href={data.site.siteMetadata.twitter} target="_blank" rel="noopener noreferrer">
+                <a href={context.twitter} target="_blank" rel="noopener noreferrer">
                     <img src="/img/social/twitter-dark.png" alt="Twitter"/>
                 </a>
             </li>
             <li>
-                <a href={data.site.siteMetadata.linkedin} target="_blank" rel="noopener noreferrer">
+                <a href={context.linkedin} target="_blank" rel="noopener noreferrer">
                     <img src="/img/social/linkedin-dark.png" alt="Linedin"/>
                 </a>
             </li>
             <li>
-                <a href={data.site.siteMetadata.github} target="_blank" rel="noopener noreferrer">
+                <a href={context.github} target="_blank" rel="noopener noreferrer">
                     <img src="/img/social/github-dark.png" alt="Github"/>
                 </a>
             </li>
