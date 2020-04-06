4b2158dddbdd120fc56ad7df96ca25d29def4027
Alejandro Vivas <alejandrojv17@gmail.com>
2020-04-06 11:35:36 -0400

    Styles improvements

--- src/components/shared/social-list.js
+++ src/components/shared/social-list.js
@@ -2,27 +2,26 @@ import React, { useContext } from 'react';
 import { ConfigContext } from './layout';
 import styles from './social-list.module.scss';
 
+const SocialItem = ({ url, label, icon }) => {
+
+    return (
+        <li>
+            <a href={url} target="_blank" rel="noopener noreferrer">
+                <img src={"/img/social/" + icon} alt={label}/>
+            </a>
+        </li>
+    )
+}
+
 const SocialList = () => {
 
     const context = useContext(ConfigContext);
 
     return (
         <ul className={styles.socialList}>
-            <li>
-                <a href={context.twitter} target="_blank" rel="noopener noreferrer">
-                    <img src="/img/social/twitter-dark.png" alt="Twitter"/>
-                </a>
-            </li>
-            <li>
-                <a href={context.linkedin} target="_blank" rel="noopener noreferrer">
-                    <img src="/img/social/linkedin-dark.png" alt="Linedin"/>
-                </a>
-            </li>
-            <li>
-                <a href={context.github} target="_blank" rel="noopener noreferrer">
-                    <img src="/img/social/github-dark.png" alt="Github"/>
-                </a>
-            </li>
+            <SocialItem url={context.twitter} label="Twitter" icon="twitter-dark.png" />
+            <SocialItem url={context.github} label="Github" icon="github-dark.png" />
+            <SocialItem url={context.linkedin} label="Linkedin" icon="linkedin-dark.png" />
         </ul>
     )
 
